5ff6d54ef9535cd2e01bac74cd2d0f11748c258d
Graham Barber <grahammbarber@gmail.com>
2018-09-15 22:45:06 -0700

    refactor(app): convert to sfc

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import { NavBar } from '../Navbar'
@@ -11,27 +11,21 @@ import { Contact } from '../Contact'
 import logo from './logo.svg'
 import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <NavBar />
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" exact component={About} />
-          <Route path="/events" exact component={Events} />
-          <Route path="/contact" exact component={Contact} />
-        </div>
-      </Router>
-    )
-  }
-}
-
-export default App
+export const App = () => (
+  <Router>
+    <div className="App">
+      <NavBar />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+      <Route path="/" exact component={Home} />
+      <Route path="/about" exact component={About} />
+      <Route path="/events" exact component={Events} />
+      <Route path="/contact" exact component={Contact} />
+    </div>
+  </Router>
+)
