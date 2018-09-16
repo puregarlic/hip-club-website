d7c01a0d07cf39ec320265ae7d4e460241490e7e
Graham Barber <grahammbarber@gmail.com>
2018-09-16 00:36:15 -0700

    feat(home): add header card

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,6 +1,25 @@
 import React from 'react'
-import { Box } from 'rebass'
+import { Flex, Card, Heading } from 'rebass'
 
-export const Home = () => {
-  return <Box bg="cyan">Home Page</Box>
-}
+import header from './header.png'
+
+export const Home = () => (
+  <div>
+    <Card
+      p={7}
+      py={9}
+      mx={5}
+      backgroundImage={`url(${header})`}
+      backgroundSize="cover"
+      backgroundPosition="center"
+      borderRadius={8}
+      boxShadow="0 2px 16px rgba(0, 0, 0, 0.25)"
+      color="white"
+      bg="darkGrey"
+    >
+      <Heading fontSize={6} textAlign="center">
+        Learn Entrepreneurial Skills and Enjoy Professional Networking Events
+      </Heading>
+    </Card>
+  </div>
+)
